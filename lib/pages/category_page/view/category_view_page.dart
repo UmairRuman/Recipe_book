de40558a6f13750789a6fe68e1b5787197595074
H4M1Z <hamizusmani1911@gmail.com>
2024-06-13 16:36:23 +0500

    Catergory app bar added

--- lib/pages/category_page/view/category_view_page.dart
+++ lib/pages/category_page/view/category_view_page.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class CategoryViewPage extends StatelessWidget {
+  const CategoryViewPage({super.key});
+  static const pageName = '/category';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) => CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              expandedHeight: constraints.maxHeight * 0.25,
+              collapsedHeight: constraints.maxHeight * 0.1,
+              pinned: true,
+              backgroundColor: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
